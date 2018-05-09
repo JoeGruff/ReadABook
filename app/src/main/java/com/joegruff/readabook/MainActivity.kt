e3a0c2b7e9fa6f451d1b03ab1036b4b50f15ad80
Joe <joegruffins@gmail.com>
2018-05-09 11:04:48 +0900

    removed some stuff and added commits

--- app/src/main/java/com/joegruff/readabook/MainActivity.kt
+++ app/src/main/java/com/joegruff/readabook/MainActivity.kt
@@ -8,28 +8,29 @@ import android.os.Bundle
 import android.support.v4.content.ContextCompat.startActivity
 import android.support.v4.content.FileProvider
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
 
 
 class MainActivity : AppCompatActivity() {
-
+    //path where pdfs are stored
+    val DIRECTORY_FOR_PDFS = "/sdcard/ebooks"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val file = File("/sdcard/ebooks");
 
+        val file = File(DIRECTORY_FOR_PDFS);
+        //get all files there then store absolute strings
         val array2 = file.listFiles()
         val array = Array<String>(array2.size, { i -> array2[i].absolutePath })
+        //adapter for listview
+        val myAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, array)
 
-        val myAdapter = ArrayAdapter(this, R.layout.filename_text_view, array)
         list_view.adapter = myAdapter
+        //selected items broadcast to pdf readers
         list_view.setOnItemClickListener { parent, view, position, id ->
-            Log.d("clicked", position.toString())
-            Log.d("bout to log", parent.getItemAtPosition(position).toString())
             val uri: Uri = FileProvider.getUriForFile(this, this.getApplicationContext().getPackageName() + ".com.joegruff.readabook.provider", File(parent.getItemAtPosition(position).toString()))
             val target = Intent(Intent.ACTION_VIEW)
             target.setDataAndType(uri, "application/pdf")
@@ -46,4 +47,5 @@ class MainActivity : AppCompatActivity() {
 
 }
 
+//needed for unique uri?
 class GenericFileProvider : FileProvider()
